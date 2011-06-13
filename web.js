5eec7ceec1b06c9e6a28fc7affd8a098caad706a
Yazad Daruvala <y.daruvala@gmail.com>
2011-06-13 05:37:20 -0700

    reimplemented with express and jade

--- web.js
+++ web.js
@@ -1,15 +1,72 @@
-//var url = require('url');
-var fs = require('fs');
-var app = require('express').createServer(), serverPort = process.env.PORT || 3000;
-
-app.get("/", function(request, response){
-	//var pathname = url.parse(request.url).pathname;
-	//if (pathname == "/") pathname = "index";
-	//console.log("Request for " + pathname + " recieved.");
-	fs.readFile("index.html", function(err, data){
-		response.send(data, {'Content-Type':'text/html'}, 200);
-	});
-});
+var express = require('express'), app = express.createServer(), serverPort = process.env.PORT || 5000;
+//app.use(express.logger());
 app.listen(serverPort, function(){
 	console.log("Listening on port " + serverPort);
 });
+
+var db_names = [
+	{name: "aaaaa"},
+	{name: "sssss"},
+	{name: "ddddd"}
+]
+
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+});
+
+app.get('/', function(req, res){
+	res.render('index', { title: "Express", users: db_names } );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var jade = require("jade");
+var fs = require('fs'), path = require('path');
+
+displayPage = function(pageName, request, response){
+	var pageName = path.join(process.cwd(), pageName + ".html");
+	path.exists(pageName, function(exists){
+		if (!exists){
+			response.send("Sorry page no longer exists" + "\n", {"Content-Type": "text/plain"}, 200);
+			return;
+		}
+		fs.readFile(pageName, function(err, data){
+			if(err) {
+				response.send(err + "\n", {"Content-Type": "text/plain"}, 500);
+				return;
+			}  
+			response.send(data, {'Content-Type':'text/html'}, 200);
+		});
+	});
+}
+
+app.get("/", function(request, response){
+	var local_var = "I am a local var";
+	jade.renderFile("views/index.jade", {locals:{local_var:local_var}}, function(err, html){
+		response.send(html);
+	});
+	//displayPage("index", request, response);
+});
+
+app.get("/About", function(request, response){
+	displayPage("About", request, response);
+});
+
+app.get("*", function(request, response){
+	response.send(404);
+});
+*/

af30887bd616ac45f67db692951b8ccc418c256a
Yazad Daruvala <y.daruvala@gmail.com>
2011-06-25 17:36:40 -0700

    Removed unnecessary

--- ideallyDB.js
+++
@@ -1,7 +0,0 @@
-exports.startupList = function(){
-	return [
-		{name: "nowjs", crunchbaseURL: "NULL", websiteURL: "http://www.nowjs.com/"},
-		{name: "heroku", crunchbaseURL: "http://www.crunchbase.com/company/heroku", websiteURL: "http://www.heroku.com/"},
-		{name: "embedly", crunchbaseURL: "http://www.crunchbase.com/company/embed-ly", websiteURL: "http://www.embed.ly/"}
-	];
-}

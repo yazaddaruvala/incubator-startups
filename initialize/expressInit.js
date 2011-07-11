864abf842a2eca54f4f442d9c5dc525817418ecb
Yazad Daruvala <y.daruvala@gmail.com>
2011-07-11 02:02:19 -0700

    Updated website. Added more info to be displayed

--- initialize/expressInit.js
+++ initialize/expressInit.js
@@ -1,14 +1,13 @@
 exports.init = function(){
-var express = require('express'), app = express.createServer();
+  var express = require('express'), app = express.createServer();
   app.configure(function(){
     app.set('views', __dirname + '/../views');
     app.set('view engine', 'ejs');
+    app.use(express.static(__dirname + '../resources/static'));
+    app.use(express.logger());
   });
 
-  app.listen( process.env.PORT || 5000, function(){
-    //console.log("Listening on port " + app.address().port);
-    //app.use(express.logger());
-  });
+  app.listen( process.env.PORT || 5000);
 
   return app;
 }
